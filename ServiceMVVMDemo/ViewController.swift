0287f3f9b98b022d181bfa39d6f48f71f541cb5e
Bharat Khatke <bharat000k@gmail.com>
2020-08-21 22:40:39 +0530

    Initial Commit

---
+++ ServiceMVVMDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ServiceMVVMDemo
+//
+//  Created by Bharat Khatke on 21/08/20.
+//  Copyright © 2020 Bharat Khatke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
